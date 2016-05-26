826353dd289d4e00501f620dd0064e855b541c6e
Sofia Nieves <xtothed@gmail.com>
2016-05-26 12:13:53 +0700

    Completed "Hello World" for Python

---
+++ python/hello-world/hello_world.py
@@ -0,0 +1,12 @@
+#
+# Skeleton file for the Python "Hello World" exercise.
+# Completed by Sofia Nieves
+# Thu 26 May 05:10:59 UTC 2016
+#
+
+
+def hello(name=''):
+    if name != '':
+        return 'Hello, ' + name + '!'
+
+    return 'Hello, World!'
