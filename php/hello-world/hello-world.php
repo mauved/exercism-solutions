9b0fab8acc99f8b451e4fe954df346560aa1f602
Sofia Nieves <xtothed@gmail.com>
2016-05-29 15:56:47 +0700

    Completed "Hello World" for PHP

---
+++ php/hello-world/hello-world.php
@@ -0,0 +1,18 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Hello World" exercise.
+// It's been provided as a convenience to get you started writing code faster.
+// Completed by Sofia Nieves
+// Sun 29 May 08:56:19 UTC 2016
+//
+
+function helloWorld($name = '')
+{
+    if ($name != '')
+    {
+        return 'Hello, ' . $name . '!';
+    }
+
+    return 'Hello, World!';
+}
