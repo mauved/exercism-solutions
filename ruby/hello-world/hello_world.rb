66372801d178565bd7a3168d9df151d76f2fd02c
Sofia Nieves <xtothed@gmail.com>
2016-06-18 00:15:03 -0500

    Solution for hello world in ruby

---
+++ ruby/hello-world/hello_world.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+#
+# Solution for Hello World
+# Written by Sofia Nieves
+# Sat 18 Jun 05:13:55 UTC 2016
+#
+
+class HelloWorld
+  def self.hello(name = 'World')
+    return 'Hello, ' + name + '!'
+  end
+end
