3f60f72f507b30317f473e0921782a853c8bf7e5
Sofia Nieves <sofia.nieves@rackspace.com>
2016-08-21 23:47:57 -0500

    Rewrote decoding function and added encoding function.
    Both functions only work for single number of characters. I need to
    add support for multidigit characters e.g. "10A".

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -1,24 +1,34 @@
 # Solution for run length
 # Written by Sofia Nieves
 
-
 def encode(word):
-    #for character in encoded_word:
-        
-    return ''
+    encoded_word = ''
+    count = 0
+    previous_character = str(word[0])
+
+    for character in word:
+        if character == previous_character:
+            count += 1
+        else:
+            encoded_word += str(count) + str(previous_character)
+            previous_character = character
+            count = 1
+    encoded_word += str(count) + str(previous_character)
+
+    return encoded_word
 
 def decode(encoded_word):
     decoded_word = ''
-    expand_char = ''
-    for character, position in encoded_word:
-        if number_characters == 0:
-            number_characters = int(character)
-            continue
-        expand_char = character
+    count = 0
 
-        while number_characters > 0:
-            decoded_word += expand_char
-            number_characters -= 1
-            
+    for character in encoded_word:
+        if count <= 0:
+            count = int(character)
+            continue
+
+        while count > 0:
+            decoded_word += character
+            count -= 1
+
     return decoded_word
 
