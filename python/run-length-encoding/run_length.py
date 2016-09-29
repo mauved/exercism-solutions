3fb5b3c477bb0f5f1877770940ef35f71f2702ec
Sofia Nieves <xtothed@gmail.com>
2016-09-28 21:51:34 -0500

    Completed decoding function and added comments

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -1,21 +1,27 @@
 # Solution for run length
 # Written by Sofia Nieves
 
+import string
 
 def encode(word):
     encoded_word = ''
     previous_character = word[0]
     character_count = 0
 
+    # if the current character matches previous character increment counter
+    # if not, append our count and the character
     for character in word:
         if character == previous_character:
             character_count += 1
         else:
+            # If the character only occurs once, the character is appended by
+            # itself e.g. AAB becomes 2AB and not 2A1B
             if character_count == 1:
                 encoded_word += previous_character
             else:
                 encoded_word += str(character_count) + previous_character
 
+            # reinitialize counter and set previous character
             previous_character = character
             character_count = 1
     
@@ -28,16 +34,27 @@ def encode(word):
 
 def decode(encoded_word):
     decoded_word = ''
-    expand_char = ''
-    for character, position in encoded_word:
-        if number_characters == 0:
-            number_characters = int(character)
-            continue
-        expand_char = character
+    count = ''
 
-        while number_characters > 0:
-            decoded_word += expand_char
-            number_characters -= 1
+    # If the character's a number append it
+    # If it isn't, use the number to repeat the character
+    for character in encoded_word:
+        if character not in string.digits:
+
+            # If we don't have a number the character is only listed once
+            if count == '':
+                count = 1
+            else:
+                count = int(count)
             
+            # append character
+            for _ in range(count):
+                decoded_word += character
+
+            count = ''
+            continue
+
+        count += character
+
     return decoded_word
 
