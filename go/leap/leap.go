52d74927ce198ad4842a1d6606c9d0c5a4fe0316
Sofia Nieves <xtothed@gmail.com>
2016-12-23 16:30:59 -0600

    updated comments for leap.go

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,13 +1,14 @@
-// Leap stub file
 // completed by Sofia Nieves
 
-// The package name is expected by the test program.
 package leap
 
 // testVersion should match the targetTestVersion in the test file.
 const testVersion = 2
 
-// It's good style to write a comment here documenting IsLeapYear.
+//Rules of leap years:
+//on every year that is evenly divisible by 4
+//  except every year that is evenly divisible by 100
+//    unless the year is also evenly divisible by 400
 func IsLeapYear(yr int) bool {
 	return (yr%4 == 0) && ((yr%100 != 0) || (yr%400 == 0))
 }
