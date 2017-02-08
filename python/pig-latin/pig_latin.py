4f7545856cf9477c3fac22096cffd7723af25680
Sofia Nieves <xtothed@gmail.com>
2017-02-07 22:39:57 -0600

    Added exceptions to the simple cases

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,8 +1,23 @@
 # Solution for Pig Latin
 
-VOWELS = 'AEIOU'
+VOWELS = 'aeiouAEIOU'
+VOWELEXCEPTIONS = ['yt', 'xr']
+EXCEPTIONS2 = ['th', 'qu', 'sh', 'ch']
+EXCEPTIONS3 = ['squ', 'thr', 'sch']
 
 def translate(en_word):
+
+    # Check for exceptions first
+    if en_word[:3] in EXCEPTIONS3:
+        return en_word[3:] + en_word[:3] + 'ay'
+    
+    if en_word[:2] in EXCEPTIONS2:
+        return en_word[2:] + en_word[:2] + 'ay'
+
+    if en_word[:2] in VOWELEXCEPTIONS:
+        return en_word + 'ay'
+
+    # These are the simple cases
     if en_word[:1] in VOWELS:
         return en_word + 'ay'
     if en_word[:1] not in VOWELS:
