720cd2dec2dd5b096d91ce0f333b7fbc9cbf78de
Sofia Nieves <xtothed@gmail.com>
2017-06-15 11:06:24 -0500

    Solution for sieve of eratosthenes in Python

---
+++ python/sieve/sieve.py
@@ -0,0 +1,26 @@
+import math
+
+
+def sieve(limit):
+
+    # If the number's too small there are no primes to check for
+    if limit < 2:
+        return []
+
+    worklist = list(range(2, limit + 1))
+    primes = []
+    notprimes = []
+
+    for i in worklist:
+        # Assume i is prime if not marked as nonprime
+        if i not in notprimes:
+            primes.append(i)
+
+            # All nonprimes will be marked after
+            # iterating past square root of limit
+            if i <= int(math.sqrt(limit)):
+                # Mark multiples of prime as nonprime
+                for j in range(i**2, limit + 1, i):
+                    notprimes.append(j)
+
+    return primes
