0f4c6667e54d3753704a0b7befc0c08f036881cc
Sofia Nieves <xtothed@gmail.com>
2017-06-21 12:00:34 -0500

    Completed robot simulator in Python

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -1,15 +1,24 @@
+# Robot bearing definitions
+NORTH = "NORTH"
+SOUTH = "SOUTH"
+EAST = "EAST"
+WEST = "WEST"
+
+
 class Robot(object):
-    NORTH = "NORTH"
-    SOUTH = "SOUTH"
-    EAST = "EAST"
-    WEST = "WEST"
 
-    def __init__(self):
-        self.coordinates = (0, 0)
-        self.bearing = NORTH
-        pass
+    def __init__(self, bearing=None, x=None, y=None):
+        """ Default bearing for robot is facing north at coordinates (0, 0) """
+        if bearing is None:
+            self.bearing = NORTH
+        else:
+            self.bearing = bearing
+        if x is None and y is None:
+            self.coordinates = (0, 0)
+        else:
+            self.coordinates = (x, y)
 
-    def turn_left():
+    def turn_left(self):
         if self.bearing == NORTH:
             self.bearing = WEST
         elif self.bearing == SOUTH:
@@ -18,9 +27,8 @@ class Robot(object):
             self.bearing = SOUTH
         elif self.bearing == EAST:
             self.bearing = NORTH
-        pass
 
-    def turn_right():
+    def turn_right(self):
         if self.bearing == NORTH:
             self.bearing = EAST
         elif self.bearing == SOUTH:
@@ -29,21 +37,25 @@ class Robot(object):
             self.bearing = NORTH
         elif self.bearing == EAST:
             self.bearing = SOUTH
-        pass
 
-    def advance():
-        current_coords = self.coordinates
+    def advance(self):
         if self.bearing == NORTH:
-            self.coordinates = (current_coords[0], current_coords[1] + 1)
+            self.coordinates = (self.coordinates[0], self.coordinates[1] + 1)
         elif self.bearing == SOUTH:
-            self.coordinates = (current_coords[0], current_coords[1] - 1)
+            self.coordinates = (self.coordinates[0], self.coordinates[1] - 1)
         elif self.bearing == WEST:
-            self.coordinates = (current_coords[0] - 1, current_coords[1])
+            self.coordinates = (self.coordinates[0] - 1, self.coordinates[1])
         elif self.bearing == EAST:
-            self.coordinates = (current_coords[0] + 1, current_coords[1])
-        pass
+            self.coordinates = (self.coordinates[0] + 1, self.coordinates[1])
 
-    def simulate(instructions):
+    def simulate(self, instructions):
+        """ Any instructions given other than advance, turn left, turn right are
+        ignored
+        """
         for step in instructions:
-            
-        pass
+            if step == "A":
+                self.advance()
+            elif step == "L":
+                self.turn_left()
+            elif step == "R":
+                self.turn_right()
