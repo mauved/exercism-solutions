bced138ce900e236470c03a41e6a85971b3bdb83
Sofia Nieves <sofia@qwfp.net>
2018-12-23 11:10:59 -0500

    Initial solution for grains in bash

---
+++ bash/grains/grains.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Not going to bother computing the total since
+# it's a constant value
+readonly BOARDTOTAL=18446744073709551615
+if [[ $1 == "total" ]]; then
+	printf %s "${BOARDTOTAL}"
+	exit 0
+fi
+
+# Standard chess boards have 64 squares
+if [[ $1 -lt 1 || $1 -gt 64 ]]; then
+	printf "Error: invalid input\n" >&2
+	exit 1
+fi
+
+# unsigned integer for handling later squares
+# such as 2^(64-1)
+printf %u "$((2**((${1}-1))))"
