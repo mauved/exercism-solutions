8aef9613edf4a86066890cf9a79527435340a539
Sofia Nieves <sofia@qwfp.net>
2018-12-23 09:53:59 -0500

    Initial solution for reverse-string in bash
    My initial solution makes use of Bash's substring extraction. I iterate
    over the string in reverse order extracting and printing one character
    at a time.

---
+++ bash/reverse-string/reverse_string.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Tested with 
+# GNU bash, version 4.4.23(1)-release (x86_64-redhat-linux-gnu)
+
+for ((i=${#1}-1;i>=0;i--));
+do
+	# Substring expansion used to extract characters in reverse order
+	# one character at a time
+	printf "${1:${i}:1}"
+done
