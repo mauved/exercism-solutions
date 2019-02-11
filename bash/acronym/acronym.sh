81d84d696a8a92076c8c0f02e14cc75e4de6e094
Sofia Nieves <sofia@qwfp.net>
2019-02-10 23:58:35 -0500

    initial soluton for acronym in bash

---
+++ bash/acronym/acronym.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Characters separated by whitespace and dashes
+# are separate words.
+IFS+="-"
+declare -a words=(${1})
+
+for word in "${words[@]}"; do
+	acronym+="${word:0:1}"
+done
+
+# Uppercase all output
+echo "${acronym^^}"
