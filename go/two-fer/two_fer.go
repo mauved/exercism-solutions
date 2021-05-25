cc5f4a1ce59dcc55dcb3639a95b5d25f2119d98f
Sofia Nieves <sofia+git@qwfp.net>
2021-05-24 21:13:33 -0400

    Initial solution for twofer in go.

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,12 @@
+/*
+Package twofer announces distribution.
+*/
+package twofer
+
+// ShareWith should have a comment documenting it.
+func ShareWith(name string) string {
+	if name == "" {
+		name = "you"
+	}
+	return "One for " + name + ", one for me."
+}
